141046907f6ae49f661ebe72aa9900258e63d0aa
svt13 <svt13@ukr.net>
2023-02-17 20:09:53 +0200

    add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div className="m-0 p-5 border-top border-1 border-light text-white text-center">
+            <p>&copy; Don't copy please</p>
+        </div>
+    );
+}
+
+export default Footer;
